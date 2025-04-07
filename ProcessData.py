33b3f34905f1bb2e15cd1b5db4e4c42734f1dcb4
ddaud23 <ddaud@unomaha.edu>
2025-04-07 21:04:20 +0000

    Note DOne

--- ProcessData.py
+++ ProcessData.py
@@ -8,16 +8,37 @@ import random
 def main():
 
   #Open the files we will be using
+
   inFile = open("names.dat", 'r')
   outFile = open("StudentList.csv", 'w')
 
+
   #Process each line of the input file and output to the CSV file
+  #line = inFile.readline()
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    school_year = data[3]
+    major = data[4]
+    idNum = data[3]
 
+    school_year_str = makeSchoolYear(school_year)
+    major_abbr = makeMajor(major)
 
+    output = last + "," + first + "," + school_year_str + "," + major_abbr + "\n"
+    outFile. write(output)
+    print(school_year_str, major_abbr)
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
 
+def makeSchoolYear(year):
+  return year.capatalize()
+
+def makeMajor(major):
+  return major[3:].capatalize()
+  
 if __name__ == '__main__':
   main()
